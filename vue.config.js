be15a555a91a4be9387397f1789a931e7037c757
Justine <hell.justine@gmail.com>
2021-07-27 13:31:44 +0200

    install dependencies

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'fr',
+      fallbackLocale: 'fr',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
+
+  transpileDependencies: [
+    'vuetify'
+  ]
+};
