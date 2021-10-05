7205008920ee61acb7f1a2739d7ea126bd875bd1
Justine <hell.justine@gmail.com>
2021-10-05 18:34:18 +0200

    Code optimization

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,17 +1,17 @@
 import { convertArrayToObject } from './index';
 
-describe('Testing convertToObject function', () => {
-  test('check if function exists', () => {
-    expect(typeof convertArrayToObject).toEqual('function');
-  });
-
+describe('Testing convertArrayToObject function', () => {
   const arr = [
     { id: 1, name: 'John' },
     { id: 2, name: 'Sarah' },
     { id: 3, name: 'Kate' },
   ];
 
-  test('Transform array into object with "id" as key', () => {
+  test('checks if function exists', () => {
+    expect(typeof convertArrayToObject).toEqual('function');
+  });
+
+  test('Transforms array into object with "id" as key', () => {
     const res = {
       1: { id: 1, name: 'John' },
       2: { id: 2, name: 'Sarah' },
@@ -21,11 +21,11 @@ describe('Testing convertToObject function', () => {
     expect(convertArrayToObject(arr, 'id')).toEqual(res);
   });
 
-  test('Transform array into object with unknown key', () => {
+  test('Transforms array into object with unknown key', () => {
     expect(convertArrayToObject(arr, 'age')).toBeUndefined();
   });
 
-  test('Transform array into object with empty array', () => {
+  test('Transforms array into object with empty array', () => {
     expect(convertArrayToObject([], 'id')).toEqual({});
   });
 });
