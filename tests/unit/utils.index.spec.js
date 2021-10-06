13c50a8302d8c8a842472ee1e6d14f3d4d519baf
Justine <hell.justine@gmail.com>
2021-10-06 11:15:21 +0200

    Testing js function with Jest Removing automatically installed test-utils lib Next : install vue testing lib for component testing

---
+++ tests/unit/utils.index.spec.js
@@ -0,0 +1,31 @@
+import { convertArrayToObject } from '@/utils/index';
+
+describe('Testing convertArrayToObject function', () => {
+  const arr = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Sarah' },
+    { id: 3, name: 'Kate' },
+  ];
+
+  test('checks if function exists', () => {
+    expect(typeof convertArrayToObject).toEqual('function');
+  });
+
+  test('Transforms array into object with "id" as key', () => {
+    const res = {
+      1: { id: 1, name: 'John' },
+      2: { id: 2, name: 'Sarah' },
+      3: { id: 3, name: 'Kate' },
+    };
+
+    expect(convertArrayToObject(arr, 'id')).toEqual(res);
+  });
+
+  test('Transforms array into object with unknown key', () => {
+    expect(convertArrayToObject(arr, 'age')).toBeUndefined();
+  });
+
+  test('Transforms array into object with empty array', () => {
+    expect(convertArrayToObject([], 'id')).toEqual({});
+  });
+});
